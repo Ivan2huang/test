d7790ded76fb0e7ba5c67caffccf42c842f9da5d
Ivan@cxagroup <549981759@qq.com>
2021-04-20 17:10:47 +0800

    33
    3

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,10 @@
+import { fromUnixTime } from 'date-fns';
 import { compose } from 'redux';
 import withAuth from '../authentication/withAuthProvider';
 import { Home } from '../modules/home';
-
+import { } fromUnixTime
 console.log('src/pages/index.jsx ');
 aaa
 bbb
+
 export default compose(withAuth)(Home);
